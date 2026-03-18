a48eea2c197ccef16b4269fd410b58d3a3e300fe
Fred Nicolls <30141109+nicollsf@users.noreply.github.com>
2026-03-18 18:31:21 +0200

    Made EXAMPLEs consistent

--- runme-EXAMPLE.py
+++ runme-EXAMPLE.py
@@ -5,20 +5,23 @@ import os
 from pathlib import Path
 
 dirname = '.'
-#ufname = os.path.join(dirname, 'fec2026_updates.xlsx')
+# Point this to your manual updates file (or use the provided example)
+ufname = os.path.join(dirname, 'fec_updates-EXAMPLE.xlsx')
+
+# Put the name of your CRS CSV data dump here
 crsnames = [
-    'EEE CSV 2 March 2026.csv'
+    'YOUR_CRS_DATA_HERE.csv'
 ]
 
 if __name__ == '__main__':
-    
-  for fname in crsnames:
-    crsfname = os.path.join(dirname, fname)
-    oxlsfname = os.path.join(dirname, Path(crsfname).stem + '_FNlatest.xlsx')
-            
-    stinfob, cinfo = parsecrsinput(crsfname, np.inf)
-    #stinfo = apply_update(stinfob, ufname)
-    stinfo = stinfob
-            
-    feco = FECCheck(stinfo)
-    feco.process(oxlsfname)
+    for fname in crsnames:
+        crsfname = os.path.join(dirname, fname)
+        oxlsfname = os.path.join(dirname, Path(crsfname).stem + '_output.xlsx')
+                
+        stinfob, cinfo = parsecrsinput(crsfname, np.inf)
+        # Uncomment the line below to apply manual overrides from the ufname Excel file
+        # stinfo = apply_update(stinfob, ufname)
+        stinfo = stinfob
+                
+        feco = FECCheck(stinfo)
+        feco.process(oxlsfname)
